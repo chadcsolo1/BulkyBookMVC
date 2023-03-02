9fe4d273b6ffecfb47a4f9df9490edfd3aed831d
chadc <chadc@ROXGWAPS>
2023-03-02 07:52:27 -0500

    Add project files.

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,141 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        //Dependency Injection For the DbContext.
+        //THis allows us to call on the DB Object using '_db.'
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            //We use the IEnumerable class to return a list of Category objects
+            //The below is an alternative to line 22, and just return View(objCategories)
+            //var objCategories = _db.Categories.ToList();
+            IEnumerable<category> objCategoryList = _unitOfWork.Category.GetAll();
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name cannot be the same as the display order");
+            }
+            if (ModelState.IsValid)
+            {
+                //Opens a seesion with the DB to add the 'obj' parameter
+                _unitOfWork.Category.Add(obj);
+                //The changes to the DB are saved with the line of code
+                _unitOfWork.Save();
+                TempData["success"] = "Category Created Successfully!";
+                //Redirects the to the Index action where an updated list of categories will be displayed
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //var categoryFromDb = _db.Categories.Find(id);
+            //**** These are a couple of options to find the category matching the id parameter entered for theis method.
+            //**** Find() is just the simplest so we used it.
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDbFirst);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name cannot be the same as the display order");
+            }
+            if (ModelState.IsValid)
+            {
+                //Opens a seesion with the DB to Update the 'obj' parameter
+                _unitOfWork.Category.Update(obj);
+                //The changes to the DB are saved with the line of code
+                _unitOfWork.Save();
+                TempData["Success"] = "Category Updated Successfully";
+                //Redirects the to the Index action where an updated list of categories will be displayed
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //var categoryFromDb = _db.Categories.Find(id);
+            //**** These are a couple of options to find the category matching the id parameter entered for theis method.
+            //**** Find() is just the simplest so we used it.
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDbFirst);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+
+            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Category.Delete(obj);
+            _unitOfWork.Save();
+            TempData["Success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,148 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // Index() will retrieve all of the cover types available from the DB
+        // and display them in a table on the cover type view
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType objCoverType)
+        {
+            //Create some validation to check that the name field is not blank
+            if (objCoverType.Name == null)
+            {
+                ModelState.AddModelError("name", "The Name cannot be empty!");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(objCoverType);
+
+                _unitOfWork.Save();
+
+                TempData["success"] = "Cover Type created successfully!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverTypeFromDbFirst);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(CoverType objCoverType)
+        {
+            //Check to make sure the Name field is not null
+            if (objCoverType.Name == null)
+            {
+                ModelState.AddModelError("name", "The Name field cannot be empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(objCoverType);
+
+                _unitOfWork.Save();
+
+                TempData["Success"] = "Category Updated Successfully!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(objCoverType);
+
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            //Validate the id is not null or 0
+            if(id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            var coverTypeFromDbFirts = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if(coverTypeFromDbFirts == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverTypeFromDbFirts);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var objCoverTypeDelete = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id==id);
+
+
+            if(objCoverTypeDelete == null)
+            {
+                return NotFound();
+            }
+            
+            _unitOfWork.CoverType.Delete(objCoverTypeDelete);
+
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Cover Type deleted Successfully!";
+
+            return RedirectToAction("Index");
+        }
+        
+
+       
+
+
+    }
+
+
+ }
